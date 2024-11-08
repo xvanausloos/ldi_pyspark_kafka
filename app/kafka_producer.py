011ed587d5634f6a3106d6f377dd54eb3d7c5c7d
xvanausloos <xavier.vanausloos@ladonneeintelligente.com>
2024-11-08 09:38:39 +0100

    chore: black reformat

--- app/kafka_producer.py
+++ app/kafka_producer.py
@@ -6,18 +6,20 @@ from kafka3.errors import KafkaTimeoutError, KafkaError
 
 logging.basicConfig(level=logging.INFO)
 
+
 class Producer:
     def __init__(self):
         self._init_kafka_producer()
 
     def _init_kafka_producer(self):
-        self.kafka_host="localhost:9092"
-        self.kafka_topic="my_topic"
+        self.kafka_host = "localhost:9092"
+        self.kafka_topic = "my_topic"
         self.producer = KafkaProducer(
-            bootstrap_servers=self.kafka_host, value_serializer=lambda v: json.dumps(v).encode(),
+            bootstrap_servers=self.kafka_host,
+            value_serializer=lambda v: json.dumps(v).encode(),
         )
 
-    def publish_to_kafka(self, message:str):
+    def publish_to_kafka(self, message: str):
         try:
             self.producer.send(topic=self.kafka_topic, value=message)
             self.producer.flush()
@@ -25,6 +27,3 @@ class Producer:
             logging.error(f"Exception {ex}")
         else:
             logging.info(f"Published message {message} into topic {self.kafka_topic}")
-
-
-
